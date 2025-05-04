972aa95aaa83ef940af4bf82d6271f00eaef6b7a
rachit976 <mukeshsoni38@gmail.com>
2025-05-04 20:08:36 +0530

    Add files via upload

---
+++ 23456.py
@@ -0,0 +1,12 @@
+CLAAS PAROT:
+self.name = name 
+self.age
+
+blu =parrot('blu ',10)
+woo=parrot('woo'15)
+
+print('bluis a {}')
+pront('woois also a{}')
+
+print('{}is(years)old')
+print('{}is {}years old'.)
